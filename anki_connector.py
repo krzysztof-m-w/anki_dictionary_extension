c2cc08d78a55881191e83c6b33ed019be08653f6
unknown <krzysztof.m.weber@gmail.com>
2023-08-28 15:57:28 +0200

    add finding model field names function to anki_connector

--- anki_connector.py
+++ anki_connector.py
@@ -4,6 +4,7 @@ class Anki_connector:
     def __init__(self, url = 'http://localhost:8765'):
         self.url = url
         self.get_model_names()
+        self.get_model_field_names()
 
     def get_model_names(self):
         request_data = {
@@ -20,6 +21,30 @@ class Anki_connector:
 
         return response.status_code
     
+    def get_model_field_names(self):
+        self.model_field_names = dict()
+        
+        for model in self.model_names:
+            request_data = {
+                "action": "modelFieldNames",
+                "version": 6,
+                "params": {
+                    "modelName": model
+                }
+            }
+
+            response = requests.post(self.url, json=request_data)
+
+            if response.status_code != 200:
+                self.model_field_names = dict()
+                return response.status_code
+            
+            self.model_field_names[model] = response.json()['result']
+
+        return 200
+    
 connector = Anki_connector()
 
-print(connector.model_names)
+
+
+print(connector.model_field_names)
