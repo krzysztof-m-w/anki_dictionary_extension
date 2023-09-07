3a11ca3b6b77f60dc7ae4f939f42d39f8b2d9f39
unknown <krzysztof.m.weber@gmail.com>
2023-09-07 17:59:27 +0200

    add leo connector function

---
+++ leo_connector.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+from leo_examples_parser import find_examples
+
+# gets the xml containing all the data from give url and using find_examples finds examples embeded on the leo website
+def leo_connector(url):
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Find the <script> element with the specified attributes
+    script_element = soup.find("script", {"type": "application/xml", "language": "xml", "data-dz-name": "searchresult"})
+
+    # Extract the content of the <script> element
+    if script_element:
+        xml_data = script_element.get_text()
+    else:
+        print("Script element not found in the HTML.")
+
+    return find_examples(xml_data)
